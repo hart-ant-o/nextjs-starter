100c50d7147e8c07beaaf9c806b976474ddef8a8
Hartanto <hartanto@globhy.com>
2025-05-14 11:46:13 +0700

    chore: setup shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
-    <div className="justify-gap-16 p-8mily-name:var(--font-geist-sans)] justify-gap-16 p-8mily-name:var(--font-geist-sans)] id-row id-row grid sm:p-20 min-h-sm:p-20">
+    <div className="flex flex-col items-center">
       <h1 className="text-center font-bold text-4xl sm:text-left">Welcome</h1>
+      <Button>Shadcn Button</Button>
     </div>
   )
 }
